11c60cd99d53dcad6070250482b85e6a85f7c3b6
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-23 22:09:56 +0200

    Update 1_👩‍🍼_Mama.py

--- pages/1_👩‍🍼_Mama.py
+++ pages/1_👩‍🍼_Mama.py
@@ -12,18 +12,6 @@ github = GithubContents(
             st.secrets["github"]["repo"],
             st.secrets["github"]["token"])
 
-with open('./config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-# Authenticator
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
-
 def main(username):
     st.write(f'Welcome *{username}*')
     
@@ -118,6 +106,34 @@ def main(username):
     else:
         st.write("Noch keine Tagebucheinträge vorhanden.")
 
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# Authenticator
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+def registration():
+    st.header("Registration")
+    new_username = st.text_input("New Username")
+    new_password = st.text_input("New Password", type="password")
+
+    if st.button("Register"):
+        # Check if username already exists
+        if new_username in config['credentials']:
+            st.error("Username already exists. Please choose a different one.")
+        else:
+            # Add new user to YAML file
+            config['credentials'][new_username] = new_password
+            with open('./config.yaml', 'w') as file:
+                yaml.dump(config, file)
+            st.success("Registration successful. You can now login.")
+
 name, authentication_status, username = authenticator.login()
 
 if authentication_status:
@@ -125,5 +141,7 @@ if authentication_status:
     main(username)
 elif authentication_status == False:
     st.error('Username/password is incorrect')
+    registration()  
 elif authentication_status == None:
     st.warning('Please enter your username and password')
+    registration()
