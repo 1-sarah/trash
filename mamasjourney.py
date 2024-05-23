e6b2773af43ee42babdd13a264f8d47e1aa3dacd
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-23 21:02:54 +0200

    Update mamasjourney.py

--- mamasjourney.py
+++ mamasjourney.py
@@ -1,29 +1,61 @@
-import streamlit as st
 import yaml
+import streamlit as st
 from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
 import pandas as pd
 from datetime import datetime, timedelta
 from github_contents import GithubContents
 
-# Page Configuration
-st.set_page_config(page_title="mamasjourney", page_icon=':ship:', layout="wide")
+# Verbindung zu GitHub initialisieren
+github = GithubContents(
+            st.secrets["github"]["owner"],
+            st.secrets["github"]["repo"],
+            st.secrets["github"]["token"])
 
-# Import pages
-from pages import mama, baby, statistiken, links
+# Load the configuration file
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-# Create a dictionary mapping page names to functions
-pages = {
-    "Mama": mama,
-    "Baby": baby,
-    "Statistiken": statistiken,
-    "Weiterführende Links": links
-}
+# Initialize the authenticator
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
-# Add a selectbox in the sidebar for page navigation
-st.sidebar.title("Navigation")
-page_selection = st.sidebar.selectbox("Select a page", options=list(pages.keys()))
+# Define the main show function
+def show():
+    # Page Configuration
+    st.set_page_config(page_title="mamasjourney", page_icon=':ship:', layout="wide")
 
-# Call the selected page function
-selected_page = pages[page_selection]
-selected_page.show()
+    # Import pages
+    from pages import mama, baby, statistiken, links
+
+    # Create a dictionary mapping page names to functions
+    pages = {
+        "Mama": mama,
+        "Baby": baby,
+        "Statistiken": statistiken,
+        "Weiterführende Links": links
+    }
+
+    # Add a selectbox in the sidebar for page navigation
+    st.sidebar.title("Navigation")
+    page_selection = st.sidebar.selectbox("Select a page", options=list(pages.keys()))
+
+    # Call the selected page function
+    selected_page = pages[page_selection]
+    selected_page.show()
+
+# Authentication and visualizing the elements
+name, authentication_status, username = authenticator.login()
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    show()
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
