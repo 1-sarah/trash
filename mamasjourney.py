d57c69e4513e5fe73977ae639bd99358d9c4b5e7
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-23 21:52:54 +0200

    Update mamasjourney.py

--- mamasjourney.py
+++ mamasjourney.py
@@ -1,22 +1,14 @@
 import yaml
 import streamlit as st
 from yaml.loader import SafeLoader
-from multiapp import Multiapp
 import streamlit_authenticator as stauth
 import pandas as pd
 from datetime import datetime, timedelta
 from github_contents import GithubContents
-from apps import mama, baby
 
 # Page Configuration
 st.set_page_config(page_title="mamasjourney", page_icon=':ship:', layout="wide")
+# Verbindung zu GitHub initialisieren
 
 st.title('mamasjourney :ship:')
 st.subheader('Herzlich Willkommen bei deinem eigenen Schwangerschaftstagebuch')
-# Multiapp
-app = Multiapp()
-app.add_app('👩‍🍼Mama', mama.app)
-app.add_app('🚼Baby', baby.app)
-app.run()
-
-
