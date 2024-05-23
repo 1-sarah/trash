af518088a7071194182db7e283d274bdc22d878a
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-23 21:15:39 +0200

    Update mamasjourney.py

--- mamasjourney.py
+++ mamasjourney.py
@@ -5,7 +5,6 @@ import streamlit_authenticator as stauth
 import pandas as pd
 from datetime import datetime, timedelta
 from github_contents import GithubContents
-
 # Page Configuration
 st.set_page_config(page_title="mamasjourney", page_icon=':ship:', layout="wide")
 # Verbindung zu GitHub initialisieren
@@ -25,30 +24,32 @@ authenticator = stauth.Authenticate(
     config['cookie']['key'],
     config['cookie']['expiry_days'],
     config['preauthorized']
-)
+   )
 
-# Import pages
-from pages import mama, baby, statistiken, links
+def show():
+    # Import pages
+    from pages import mama, baby, statistiken, links
+    # Create a dictionary mapping page names to functions
+    pages = {
+        "Mama": mama,
+        "Baby": baby,
+        "Statistiken": statistiken,
+        "Weiterführende Links": links
+    }
 
-# Create a dictionary mapping page names to functions
-pages = {
-    "Mama": mama,
-    "Baby": baby,
-    "Statistiken": statistiken,
-    "Weiterführende Links": links
-}
-
-# Add a selectbox in the sidebar for page navigation
-st.sidebar.title("Navigation")
-page_selection = st.sidebar.selectbox("Select a page", options=list(pages.keys()))
-# Call the selected page function
-selected_page = pages[page_selection]
+    # Add a selectbox in the sidebar for page navigation
+    st.sidebar.title("Navigation")
+    page_selection = st.sidebar.selectbox("Select a page", options=list(pages.keys()))
+    # Call the selected page function
+    selected_page = pages[page_selection]
+    selected_page.show()
 
 # Authentication and visualizing the elements
 name, authentication_status, username = authenticator.login()
 if authentication_status:
     authenticator.logout('Logout', 'main')
     st.write(f'Welcome *{name}*')
+    show()
 elif authentication_status == False:
     st.error('Username/password is incorrect')
 elif authentication_status == None:
