5818c1228630b71558418ef9592679eba38dd371
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-24 18:12:30 +0200

    Update mamasjourney.py

--- mamasjourney.py
+++ mamasjourney.py
@@ -41,7 +41,7 @@ def registration():
                 yaml.dump(config, file)
             st.success("Registration successful. You can now login.")
 
-from pages import 👩‍🍼_Mama,🚼_Baby,📈_Statistiken,🔗_Links
+from pages import _Mama,_Baby,_Statistiken,_Links
 page_names_to_funcs = {
     "1_👩‍🍼_Mama": mama,
     "2_🚼_Baby": baby,
